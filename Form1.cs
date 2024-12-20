abe769efd16bf2491a3192b8bb88be8926a34c42
Nahilor <Nahilor@DESKTOP-TQJ4JQR>
2024-12-20 11:26:09 -0800

    Changed name and size of Window

--- Form1.cs
+++ Form1.cs
@@ -6,5 +6,10 @@ namespace WeatherApp
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Console.WriteLine("HI");
+        }
     }
 }
