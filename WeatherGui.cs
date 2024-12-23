0a5170533d7e8f6657169f6b622b103ce9e3c376
Nahilor <Nahilor@DESKTOP-TQJ4JQR>
2024-12-23 21:29:51 +0300

    Revert "Changed file names from Form1 to WeatherApp and set a size to 300, 500 and changed FormBorderStyle to None."
    This reverts commit c1baf6bcdf63cf15de28c071830e0b259d55cc74.

--- WeatherGui.cs
+++ WeatherGui.cs
@@ -8,12 +8,49 @@ namespace WeatherApp
             FormBorderStyle = FormBorderStyle.None;
             Size = new System.Drawing.Size(300, 500);
             InitGui();
+            Console.WriteLine("HI");
         }
 
         public void InitGui()
         {
-
+            // Hamburger menu bag
+            ImageBtn sidebarbtn = new ImageBtn();
+            sidebarbtn.setImage("C:\\Users\\Nahilor\\Downloads\\menu.png", 30, 30);
+            sidebarbtn.MouseClick += new MouseEventHandler((sender, e) =>
+            {
+                if (e.Button== MouseButtons.Left)
+                   MessageBox.Show("Hamburger pressed");
+            });
+            sidebarbtn.Location = new Point(20, 20);
+            this.Controls.Add(sidebarbtn);
         }
 
+        class ImageBtn : PictureBox
+        {
+            Bitmap? icon;
+            public void setImage(String fileDestination, int xSize, int ySize)
+            {
+                if (icon != null) 
+                {
+                    icon.Dispose();
+                }
+
+                SizeMode = PictureBoxSizeMode.StretchImage;
+                icon = new Bitmap(fileDestination);
+                ClientSize = new Size(xSize, ySize);
+                Image = (Image) icon;
+            }
+            public void setImage(String fileDestination)
+            {
+                if (icon != null)
+                {
+                    icon.Dispose();
+                }
+
+                SizeMode = PictureBoxSizeMode.AutoSize;
+                icon = new Bitmap(fileDestination);
+                Image = (Image)icon;
+            }
+        }
     }
 }
